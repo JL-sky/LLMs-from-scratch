a20d966f5f222ef3aea266dd4b1e72faa9bf179c
jl-sky <k849683539@163.com>
2026-05-11 13:03:08 +0800

    complate ch07 with data process

---
+++ ch07/downloadDateset.py
@@ -0,0 +1,54 @@
+import json
+import os
+import requests
+
+
+def download_and_load_file(file_path, url):
+    if not os.path.exists(file_path):
+        response = requests.get(url, timeout=30)
+        response.raise_for_status()
+        text_data = response.text
+        with open(file_path, "w", encoding="utf-8") as file:
+            file.write(text_data)
+
+    with open(file_path, "r", encoding="utf-8") as file:
+        data = json.load(file)
+
+    return data
+
+
+# The book originally used the following code below
+# However, urllib uses older protocol settings that
+# can cause problems for some readers using a VPN.
+# The `requests` version above is more robust
+# in that regard.
+
+"""
+import urllib
+
+def download_and_load_file(file_path, url):
+
+    if not os.path.exists(file_path):
+        with urllib.request.urlopen(url) as response:
+            text_data = response.read().decode("utf-8")
+        with open(file_path, "w", encoding="utf-8") as file:
+            file.write(text_data)
+
+    else:
+        with open(file_path, "r", encoding="utf-8") as file:
+            text_data = file.read()
+
+    with open(file_path, "r", encoding="utf-8") as file:
+        data = json.load(file)
+
+    return data
+"""
+if __name__ =="__main__":
+    file_path = "instruction-data.json"
+    url = (
+        "https://raw.githubusercontent.com/rasbt/LLMs-from-scratch"
+        "/main/ch07/01_main-chapter-code/instruction-data.json"
+    )
+
+    data = download_and_load_file(file_path, url)
+    print("Number of entries:", len(data))
